f39c80416c969f84df2d3104660665699bc8eae4
Benjamin <bhainaut@ulb.ac.be>
2018-04-25 21:03:18 +0200

    Started working on the planck

--- ProjetRecode/src/Model/Hole.java
+++ ProjetRecode/src/Model/Hole.java
@@ -1,13 +1,34 @@
 package Model;
 
-public class Hole extends Block{
+public class Hole extends Block implements Activable{
+	private boolean plancked  = false;
 	
 	public Hole(int X, int Y) {
 		super(X,Y,"H");
 	}
-	
+
+	/*
+	 *Est-ce qu'on fait en sorte que la planche reste sur le block de vide tant que le joueur ne l'a pas récupéré ?
+	 *
+	 */
 	@Override
 	public boolean isObstacle(Entity entity) {
+		if(plancked) {
+			return false;
+		}
+		else {
 		return true;
+		}
+	}
+
+	@Override
+	public void activate() {
+		if(plancked) {
+			plancked = false;
+		}
+		else {
+			plancked = true;
+		}
+		
 	}
 }
